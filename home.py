1fa26013afdfed00e12003d1d4595164061c2158
Sachi Goel <67666178+computer-s-2@users.noreply.github.com>
2025-01-30 17:35:12 -0500

    fixed file names

---
+++ home.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import sennebogen as sennebogen
+
+dark_yellow_css = """
+<style>
+body {
+    background-color: #333300; 
+    color: #FFD700; 
+}
+
+subheader {
+    color: white;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    color: #FFCC00; 
+}
+
+.stButton>button {
+    background-color: #FFD700;
+    color: black;
+    border-radius: 5px;
+    border: 1px solid #FFCC00;
+    font-size: 16px;
+    font-weight: bold;
+}
+
+.stButton>button:hover {
+    background-color: #FFCC33; 
+    color: #333300;
+}
+
+
+.stSidebar {
+    background-color: #4D4D00; 
+    color: #FFD700;
+}
+
+.stSidebar .css-1v3fvcr {
+    color: #FFD700; 
+}
+
+input, textarea {
+    background-color: #4D4D00; 
+    color: #FFD700;
+    border: 1px solid #FFCC00;
+    border-radius: 5px;
+}
+
+a, .css-1fv8s86, .css-1hxh2wk, .css-1ynw3xd {
+    color: #FFD700; 
+}
+</style>
+"""
+st.markdown(dark_yellow_css, unsafe_allow_html=True)
+
+st.title('PDF to Excel')
+st.subheader("Input in your Sennebogen file to turn into an Excel file.", divider="gray")
+file = st.file_uploader("Input a PDF file", type=["PDF"], key="2")
+if st.button("Process File", key="2b"):
+    if file is not None:
+        with st.spinner("Processing..."):
+            excel_data = sennebogen.extract_tables_(file)
+
+            if excel_data:
+                st.download_button(
+                    label="Download Excel File",
+                    data=excel_data,
+                    file_name=f"{file.name}.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+            else:
+                st.error("No tables found in the PDF.")
+    else:
+        st.warning("Please upload a file before submitting.")
+
+st.text("Below is a work in progress...")
+st.subheader("Input in your Liebherr file to turn into an Excel file.", divider="gray")
+file = st.file_uploader("Input a PDF file", type=["PDF"], key="3")
+
+if st.button("Process File", key="3b"):
+    if file is not None:
+        with st.spinner("Processing..."):
+            excel_data = sennebogen.extract_tables_(file)
+
+            if excel_data:
+                st.download_button(
+                    label="Download Excel File",
+                    data=excel_data,
+                    file_name=f"{file.name}.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+            else:
+                st.error("No tables found in the PDF.")
+    else:
+        st.warning("Please upload a file before submitting.")
--- sennebogen.py
+++ sennebogen.py
@@ -1,96 +1,175 @@
-import streamlit as st
-import sennebogen as sennebogen
-
-dark_yellow_css = """
-<style>
-body {
-    background-color: #333300; 
-    color: #FFD700; 
-}
-
-subheader {
-    color: white;
-}
-
-h1, h2, h3, h4, h5, h6 {
-    color: #FFCC00; 
-}
-
-.stButton>button {
-    background-color: #FFD700;
-    color: black;
-    border-radius: 5px;
-    border: 1px solid #FFCC00;
-    font-size: 16px;
-    font-weight: bold;
-}
-
-.stButton>button:hover {
-    background-color: #FFCC33; 
-    color: #333300;
-}
+import pdfplumber
+import pandas as pd
+from tqdm import tqdm
+from io import BytesIO
+from openpyxl.styles import Alignment, Font, Border, Side
 
 
-.stSidebar {
-    background-color: #4D4D00; 
-    color: #FFD700;
-}
+def extract_tables_(pdf_path):
+    combined_data = []
+    previous_row = None
 
-.stSidebar .css-1v3fvcr {
-    color: #FFD700; 
-}
+    with pdfplumber.open(pdf_path) as pdf:
+        col_count = 0
+        for page_number, page in enumerate(tqdm(pdf.pages, desc="Processing Pages")):
+            try:
+                page_text = page.extract_text()
+                print(page_text.splitlines()[1] + " " + str(page_number))
+                if page_text and "Inhaltsverzeichnis" in page_text:
+                    continue
+                
+                elif page_text and "Pos./" in page_text.splitlines()[1] and "MPOS" in page_text.splitlines()[1]:
+                    debug_pic = page.to_image()
+                    debug_pic.debug_tablefinder(
+                        table_settings={
+                            "join_tolerance": 7,  
+                            "intersection_tolerance": 8,  
+                            "horizontal_strategy": "lines_strict",
+                            "snap_x_tolerance": 5,
+                            "explicit_vertical_lines": [40, 70, 110, 220, 340, 380, 510, 550],
+                            "explicit_horizontal_lines": [800]
+                        }
+                    )
+                elif page_text and "Pos./" in page_text.splitlines()[1]:
+                    # debug_pic = page.to_image()
+                    # debug_pic.debug_tablefinder(
+                    #     table_settings={
+                    #         "join_tolerance": 7,  
+                    #         "intersection_tolerance": 8,  
+                    #         "horizontal_strategy": "lines_strict",
+                    #         "snap_x_tolerance": 5,
+                    #         "explicit_vertical_lines": [40, 70, 110, 220, 330, 370, 410, 510, 550],
+                    #         "explicit_horizontal_lines": [800]
+                    #     }
+                    # )
+                    # debug_image_path = f"output_tables/debug_page_{page_number+1}.png"
+                    # debug_pic.save(debug_image_path)
 
-input, textarea {
-    background-color: #4D4D00; 
-    color: #FFD700;
-    border: 1px solid #FFCC00;
-    border-radius: 5px;
-}
+                    tables = page.extract_tables(table_settings= {
+                        "join_tolerance": 7,  
+                        "intersection_tolerance": 8,  
+                        "horizontal_strategy": "lines_strict",
+                        "snap_x_tolerance": 5,
+                        "explicit_vertical_lines": [40, 70, 110, 220, 330, 370, 410, 510, 550]
+                    })
 
-a, .css-1fv8s86, .css-1hxh2wk, .css-1ynw3xd {
-    color: #FFD700; 
-}
-</style>
-"""
-st.markdown(dark_yellow_css, unsafe_allow_html=True)
+                elif page_text and ("ME/" in page_text.splitlines()[1] or "Qty. Unit/" in page_text.splitlines()[1]):
+                    tables = page.extract_tables(table_settings={"join_tolerance": 7,  
+                                                            "intersection_tolerance": 8,  
+                                                            "horizontal_strategy": "lines_strict",
+                                                            "snap_x_tolerance": 5,
+                                                            "explicit_vertical_lines": [40, 70, 110, 220, 330, 365, 410, 505, 550],
+                                                            "explicit_horizontal_lines": [800]
+                                                            })
+                elif page_text and "Fig./" in page_text.splitlines()[1]:
+                    tables = page.extract_tables(table_settings= {
+                        "join_tolerance": 7,  
+                        "intersection_tolerance": 8,  
+                        "horizontal_strategy": "lines_strict",
+                        "snap_x_tolerance": 5,
+                        "explicit_vertical_lines": [40, 70, 110, 220, 340, 380, 510, 550]
+                    })
+                elif page_text and "Comment" in page_text.splitlines()[0]:
+                    tables = page.extract_tables(table_settings={"join_tolerance": 7,  
+                                                            "intersection_tolerance": 8,  
+                                                            "horizontal_strategy": "lines_strict",
+                                                            "snap_x_tolerance": 5,
+                                                            "explicit_vertical_lines": [35, 100, 235, 365, 440, 570],
+                                                            "explicit_horizontal_lines": [800]
+                                                            })
+                elif page_text and "Comment" in page_text.splitlines()[1]:
+                    tables = page.extract_tables(table_settings={"join_tolerance": 7,  
+                                                            "intersection_tolerance": 8,  
+                                                            "horizontal_strategy": "lines_strict",
+                                                            "snap_x_tolerance": 5,
+                                                            "explicit_vertical_lines": [50, 100, 235, 365, 440, 550],
+                                                            "explicit_horizontal_lines": [800]
+                                                            })
+                else:
+                    tables = page.extract_tables(table_settings={"join_tolerance": 7,  
+                                                            "intersection_tolerance": 8,  
+                                                            "horizontal_strategy": "lines_strict",
+                                                            "snap_x_tolerance": 5,
+                                                            "explicit_vertical_lines": [50, 100, 235, 460, 550],
+                                                            "explicit_horizontal_lines": [800]
+                                                            })
 
-st.title('PDF to Excel')
-st.subheader("Input in your Sennebogen file to turn into an Excel file.", divider="gray")
-file = st.file_uploader("Input a PDF file", type=["PDF"], key="2")
-if st.button("Process File", key="2b"):
-    if file is not None:
-        with st.spinner("Processing..."):
-            excel_data = sennebogen.extract_tables_(file)
+                if tables:   
+                    for i, table in enumerate(tables): 
+                        print(f"Table {i+1}: Columns: {len(table[0])}, Rows: {len(table) - 1}")
+                        col_count = len(table[0])                       
+                        df = pd.DataFrame(table[1:], columns=table[0])
 
-            if excel_data:
-                st.download_button(
-                    label="Download Excel File",
-                    data=excel_data,
-                    file_name=f"{file.name}.xlsx",
-                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                )
-            else:
-                st.error("No tables found in the PDF.")
-    else:
-        st.warning("Please upload a file before submitting.")
+                        data = df.values.tolist()
+                        processed_data = []
+                        
+                        for row in data:
+                            if not row[0].strip() or not row[0].isdigit():
+                                if previous_row:
+                                    for col_index in range(1, len(row)):
+                                        previous_row[col_index] += f" {row[col_index]}".strip()
+                                else:
+                                    print(f"Warning: Overflow row detected without a previous row: {row}")
+                            else:
+                                processed_data.append(row)
+                                previous_row = row  
 
-st.text("Below is a work in progress...")
-st.subheader("Input in your Liebherr file to turn into an Excel file.", divider="gray")
-file = st.file_uploader("Input a PDF file", type=["PDF"], key="3")
+                        combined_data.extend(processed_data)
 
-if st.button("Process File", key="3b"):
-    if file is not None:
-        with st.spinner("Processing..."):
-            excel_data = sennebogen.extract_tables_(file)
+            except Exception as e:
+                print(f"Error on pathge {page_number+1}: {e}")
+    combined_df = ""
+    output_stream = ""
+    col_count = len(combined_data[0])
+    print(f"First few rows of combined_data: {combined_data[:5]}")
+    if col_count == 5 or col_count == 4:
+        combined_df = pd.DataFrame(combined_data, columns=["Item no.", "SeboNr", "Description", "Quantity", "Comment"])
+        output_stream = BytesIO()
+    elif col_count == 7:
+        combined_df = pd.DataFrame(combined_data, columns=["Pos./\nFig.", "Ident./\nNo.", "Benennung", "Nomenclature", "Menge/\nQty.", "MPOS~Bemerkung/\nRemark", "s. Seite\nsee Page"])
+        output_stream = BytesIO()
+    elif col_count == 8:
+        combined_df = pd.DataFrame(combined_data, columns=["Fig./\nPos.", "No./\nIdent.", "Nomenclature", "Benennung", "Qty./\nMenge.", "Qty. Unit/", "MPOS~Remark/Bemerkung", "see page\n s. Seite"])
+        output_stream = BytesIO()
+    # make excel pretty
+    with pd.ExcelWriter(output_stream, engine='openpyxl') as writer:
+        combined_df.to_excel(writer, index=False, sheet_name='Combined Output')
+    
+    output_stream.seek(0)
+    from openpyxl import load_workbook
+    wb = load_workbook(output_stream)
+    sheet = wb.active
 
-            if excel_data:
-                st.download_button(
-                    label="Download Excel File",
-                    data=excel_data,
-                    file_name=f"{file.name}.xlsx",
-                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                )
-            else:
-                st.error("No tables found in the PDF.")
-    else:
-        st.warning("Please upload a file before submitting.")
+    for column in sheet.columns:
+        max_length = 0
+        column_letter = column[0].column_letter 
+        for cell in column:
+            try:
+                if cell.value:
+                    max_length = max(max_length, len(str(cell.value)))
+            except Exception:
+                pass
+        adjusted_width = max_length + 2  
+        sheet.column_dimensions[column_letter].width = adjusted_width
+
+    for cell in sheet[1]: 
+        cell.font = Font(bold=True)
+
+    for row in sheet.iter_rows():
+        for cell in row:
+            cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    thin_border = Border(
+        left=Side(style='thin'),
+        right=Side(style='thin'),
+        top=Side(style='thin'),
+        bottom=Side(style='thin')
+    )
+    for row in sheet.iter_rows():
+        for cell in row:
+            cell.border = thin_border
+
+    output_stream = BytesIO()
+    wb.save(output_stream)
+    output_stream.seek(0)
+    return output_stream
